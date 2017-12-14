bf21f83e6f662e35aaf692c53d7d520e463b7e70
pengxuanlan <pxl19870821>
2017-12-14 10:52:21 +0800

    作业二

--- day2/test2.py
+++ day2/test2.py
@@ -1,22 +1,7 @@
-exit=False
-f=open('user','r+',encoding="utf-8").read()
-file=str(f)
-for line in file:
-    file_str=str(file)   #转成字符串
-data=eval(file_str)   #转换成字典
-name=input("请输入用户姓名：")
-password=input("请输入密码：")
-while True:
-    if name in data:
-        if password in data[name]:
-            print("登录成功！")
-            break
+# f 打开用户文件
 
-        else:
-            print("密码错误，登录失败")
-            continue
-    else:
-        print("您输入的用户名不存在，请重试")
-        continue
-
-
+set=False
+f=str(open("user","r+",encoding="utf-8").read())
+for line in f:
+    file_str=str(f)
+print (file_str)
---
+++ day2/test3.py
@@ -0,0 +1,12 @@
+#Author:pjf
+#github=https://github.com/pengxuanlan/python
+#time:2017-12-14 10:44
+
+set=False
+f=str(open("user","r+",encoding="utf-8").read())
+for line in f:
+    file_str=str(f)
+data=eval(file_str)   #字符串转换成字典
+
+
+
